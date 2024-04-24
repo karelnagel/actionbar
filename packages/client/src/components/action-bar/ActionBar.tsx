642e15ee811af2dff8a7b37306635530869dde58
Karel <nagelkarel@gmail.com>
2024-04-24 11:58:16 +0300

    vercel design

--- packages/client/src/components/action-bar/ActionBar.tsx
+++ packages/client/src/components/action-bar/ActionBar.tsx
@@ -1,6 +1,7 @@
-import { useEffect, useRef } from "react";
+import { Fragment, useEffect, useRef } from "react";
 import { useStore } from "@nanostores/react";
 import { actionBarChatMode, actionBarOpen, actionBarSearch, actionBarSelectedIndex } from "./state";
+import { ArrowUpRight, LucideIcon, FileIcon } from "lucide-react";
 
 export const useKeyboardShortcuts = () => {
   useEffect(() => {
@@ -33,12 +34,13 @@ type Section = {
 type Item = {
   title: string;
   href?: string;
+  Icon?: LucideIcon;
+  cta?: string;
   action?: (() => void) | (() => Promise<void>);
-  askAI?: boolean;
 };
 
 const POSTS: Item[] = [
-  { title: "Cats", href: "/cats" },
+  { title: "Cats", href: "/cats", Icon: FileIcon },
   { title: "Dogs", href: "/dogs" },
   { title: "Rats", href: "/rats" },
   { title: "Bats", href: "/bats" },
@@ -79,14 +81,11 @@ const SECTIONS: Section[] = [
       return items;
     },
   },
-  { title: "AI", items: [{ title: "Ask AI", askAI: true }] },
 ];
 
 const compare = (a: string, b: string) => a.toLowerCase().includes(b.toLowerCase());
-type ActionBarProps = {
-  hideSections?: boolean;
-};
-export const ActionBar = ({ hideSections }: ActionBarProps) => {
+type ActionBarProps = {};
+export const ActionBar = ({}: ActionBarProps) => {
   const open = useStore(actionBarOpen);
   const search = useStore(actionBarSearch);
   const selectedIndex = useStore(actionBarSelectedIndex);
@@ -102,29 +101,44 @@ export const ActionBar = ({ hideSections }: ActionBarProps) => {
   }, [actionBarSelectedIndex]);
   return (
     <div
-      style={{ display: open ? "flex" : "none" }}
-      className="absolute left-0 top-0 h-full w-full items-center justify-center"
+      style={{
+        display: open ? "flex" : "none",
+        background: open ? "rgba(0,0,0,0.2)" : "none",
+        colorScheme: "dark",
+      }}
+      className="absolute left-0 top-0 h-full w-full items-center justify-center p-4 duration-150"
       onClick={() => actionBarOpen.set(false)}
     >
-      <div className="rounded-md bg-blue-300 p-10 " onClick={(e) => e.stopPropagation()}>
-        <input
-          ref={inputRef}
-          type="text"
-          value={search}
-          onChange={(e) => actionBarSearch.set(e.target.value)}
-        />
-        <div className="flex flex-col">
+      <div
+        className="w-full max-w-[700px] rounded-2xl border border-white/15 bg-[#121212] text-white"
+        onClick={(e) => e.stopPropagation()}
+      >
+        <div className="p-3">
+          <input
+            ref={inputRef}
+            type="text"
+            className="placeholder:text-current/50 bg-transparent text-xl focus:outline-none"
+            value={search}
+            placeholder="What do you need?"
+            onChange={(e) => actionBarSearch.set(e.target.value)}
+          />
+        </div>
+        <div className="h-[1px] w-full bg-white/15"></div>
+        <div className="flex max-h-[300px] flex-col overflow-y-scroll p-3">
           {SECTIONS.map((section) => (
-            <div key={section.title}>
-              {!hideSections && <p className="text-sm font-semibold uppercase">{section.title}</p>}
-              <div className="flex flex-col items-start gap-1">
-                {section.items
-                  ?.filter((item) => compare(item.title, search))
-                  .map((item, i) => (
-                    <ActionItem selected={selectedIndex === i} key={item.title} {...item} />
-                  ))}
-              </div>
-            </div>
+            <Fragment key={section.title}>
+              <p className="py-1 text-sm text-white/50">{section.title}</p>
+              {section.items
+                ?.filter((item) => compare(item.title, search))
+                .map((item, i) => (
+                  <ActionItem
+                    selected={selectedIndex === i}
+                    key={item.title}
+                    item={item}
+                    index={i}
+                  />
+                ))}
+            </Fragment>
           ))}
         </div>
       </div>
@@ -132,32 +146,41 @@ export const ActionBar = ({ hideSections }: ActionBarProps) => {
   );
 };
 
-const ActionItem = (item: Item & { selected: boolean }) => {
+const ActionItem = ({
+  item,
+  selected,
+  index,
+}: {
+  item: Item;
+  selected: boolean;
+  index: number;
+}) => {
+  const action = () => {
+    if (item.action) item.action();
+    if (item.href) window.location.href = item.href;
+  };
+
   useEffect(() => {
     const listenToEnter = (e: any) => {
-      const open = actionBarOpen.get();
-      if (!open) return;
-
-      if (e.key === "Enter") {
-        if (item.action) item.action?.();
-        else if (item.askAI) actionBarChatMode.set(true);
-        else if (item.href) window.location.href = item.href;
-      }
+      if (actionBarOpen.get() && e.key === "Enter") action();
     };
     document.addEventListener("keydown", listenToEnter);
     return () => document.removeEventListener("keydown", listenToEnter);
   }, []);
-
-  if ("href" in item)
-    return (
-      <a style={{ background: item.selected ? "red" : undefined }} href={item.href}>
-        {item.title}
-      </a>
-    );
-
+  const Icon = item.Icon || ArrowUpRight;
   return (
-    <button style={{ background: item.selected ? "red" : undefined }} onClick={item.action}>
-      {item.title}
+    <button
+      onMouseEnter={() => actionBarSelectedIndex.set(index)}
+      className={`${selected ? "bg-white/10 text-white" : "text-white/70"} flex items-center gap-2 rounded-md p-2 text-[15px] duration-150`}
+      onClick={action}
+    >
+      <Icon className="h-5 w-5" />
+      <span>{item.title}</span>
+      <span
+        className={`${selected ? "opacity-60" : "opacity-0"} ml-auto rounded-md  p-1 text-xs duration-150`}
+      >
+        {item.cta || "Go to"}
+      </span>
     </button>
   );
 };
