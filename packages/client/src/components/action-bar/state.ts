01822017d3bad222f4063756ce28c7e197c985eb
Karel <nagelkarel@gmail.com>
2024-04-24 20:19:24 +0300

    .

--- packages/client/src/components/action-bar/state.ts
+++ packages/client/src/components/action-bar/state.ts
@@ -4,8 +4,8 @@ import { ActionBarElement, ActionBarSections } from "./types";
 export const actionBarOpen = atom(true);
 export const actionBarSearch = atom("");
 export const actionBarSelectedId = atom<string | null>(null);
-export const actionBarChatMode = atom(false);
 export const actionBarVisibleSections = map<ActionBarSections>({});
+
 export const actionBarElements = computed(actionBarVisibleSections, (sections) => {
   const elements: ActionBarElement[] = Object.values(sections)
     .filter((section) => section.items.length)
@@ -15,6 +15,7 @@ export const actionBarElements = computed(actionBarVisibleSections, (sections) =
     ]);
   return elements;
 });
+
 export const actionBarItems = computed(actionBarElements, (elements) => {
   return elements.filter((x) => x.type === "item").map((x) => (x.type === "item" ? x.item : null)!);
 });
