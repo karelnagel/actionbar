9bd7e07898d7afe7ddb340c15a48b365530db943
Karel <nagelkarel@gmail.com>
2024-05-02 14:45:44 +0300

    better

--- packages/knowledge/src/html-to-md.ts
+++ packages/knowledge/src/html-to-md.ts
@@ -3,6 +3,7 @@ import TurndownService from "turndown";
 const fixUrl = (url: string, pageUrl: URL) => {
   url = url.replaceAll(" ", "%20");
   if (url.startsWith("/")) return pageUrl.origin + url;
+  if (url.startsWith("#")) return pageUrl.toString() + url;
   return url;
 };
 
@@ -30,26 +31,37 @@ export const htmlToMD = (html: string, url: URL) => {
   });
   converter.addRule("video", {
     filter: ["video", "audio", "img"],
-    replacement: (content, node) => {
-      const src = (node as unknown as { src: string }).src;
+    replacement: (content, node: any) => {
+      const src = node.src;
+      const alt = node.alt;
       if (!src) return content;
-      return `![${content}](${fixUrl(src, url)})`;
+      return `![${content || alt || ""}](${fixUrl(src, url)})`;
     },
   });
   converter.addRule("footerheader", {
     filter: ["footer", "header", "nav", "aside", "section", "article", "main"],
-    replacement: (content, node) => {
-      const tag = (node as unknown as { tagName: string }).tagName.toLowerCase();
+    replacement: (content, node: any) => {
+      const tag = node.tagName.toLowerCase();
+      const id = node.id;
       content = content
         .split("\n")
         .filter((x) => x.trim())
         .join("\n\n");
-      return `\n\n<${tag}>\n\n${content}\n\n</${tag}>\n\n`;
+      return `\n\n<${tag} ${id ? `id="${id}"` : ""}>\n\n${content}\n\n</${tag}>\n\n`;
     },
   });
   converter.addRule("hide", {
     filter: ["script", "style", "link", "meta", "iframe", "noscript"],
     replacement: () => "",
+  });
+  converter.addRule("headings", {
+    filter: ["h1", "h2", "h3", "h4", "h5", "h6"],
+    replacement: (content, node: any) => {
+      const heading = node.tagName.toLowerCase();
+      const id = node.id;
+      if (id) return `\n<${heading} id="${id}">${content}</${heading}>\n`;
+      return `${"#".repeat(Number(heading.slice(1)))} ${content}`;
+    },
   });
   return converter.turndown(html);
 };
