be424ec04de56811d8992c98aba3300752a464b7
Karel <nagelkarel@gmail.com>
2024-05-02 10:02:20 +0300

    trying html to md

--- packages/knowledge/src/sitemap.ts
+++ packages/knowledge/src/sitemap.ts
@@ -1,5 +1,7 @@
 import { RecursiveCharacterTextSplitter } from "langchain/text_splitter";
 import * as cheerio from "cheerio";
+import fs from "fs";
+import TurndownService from "turndown";
 
 export const getPagesFromSitemap = async (url: string): Promise<string[]> => {
   const res = await fetch(url);
@@ -11,19 +13,35 @@ export const getPagesFromSitemap = async (url: string): Promise<string[]> => {
   return sites;
 };
 
-export const getPage = async (url: string) => {
-  const html = await fetch(url).then((res) => res.text());
-  const $ = cheerio.load(html);
+export const getPage = async (href: string) => {
+  const url = new URL(href);
+  const res = await fetch(url).then((res) => res.text());
+  const $ = cheerio.load(res);
   $("script").remove();
   const title = $("title").text() || "";
   const description = $("meta[name=description]").attr("content") || "";
-  const text = $("body").text();
+  const html = $("body").html() || "";
+  const converter = new TurndownService({
+    headingStyle: "atx",
+    fence: "```",
+    linkStyle: "inlined",
+    codeBlockStyle: "fenced",
+    defaultReplacement: (content) => {
+      const origin = url.origin;
+      const res = content
+        .replaceAll(/\((?!https:\/\/)/g, `(${origin}/`)
+        .replace(/([^:]\/)\/+/g, "$1");
+      return res;
+    },
+  });
+  const markdown = converter.turndown(html);
   const splitter = new RecursiveCharacterTextSplitter({
     chunkSize: 512,
     chunkOverlap: 40,
   });
-  const output = await splitter.createDocuments([text]);
+  const output = await splitter.createDocuments([markdown]);
   return {
+    markdown,
     texts: output.map((doc) => doc.pageContent),
     title,
     description,
@@ -40,3 +58,12 @@ export const indexSite = async (url: string) => {
   }
   return indexed;
 };
+
+fs.mkdirSync("out", { recursive: true });
+getPage("https://wolfagency.ee/").then((x) => fs.writeFileSync("out/wolfagency.md", x.markdown));
+getPage("https://asius.ai").then((x) => fs.writeFileSync("out/asius.md", x.markdown));
+getPage("https://astro.build").then((x) => fs.writeFileSync("out/astro.md", x.markdown));
+getPage("https://ion.sst.dev/docs/component/aws/astro/").then((x) =>
+  fs.writeFileSync("out/ion.md", x.markdown),
+);
+getPage("https://www.framer.com/").then((x) => fs.writeFileSync("out/framer.md", x.markdown));
